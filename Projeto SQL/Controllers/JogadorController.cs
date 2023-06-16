777cfc43f93a744f468402e1d4baddbd4ea5db71
Guilherme Luiz Da Silva <36992072854@info.local>
2023-06-16 17:20:43 -0300

    projeto-gamer-finalizado

---
+++ Projeto SQL/Controllers/JogadorController.cs
@@ -0,0 +1,108 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Projeto_SQL.Infra;
+using Projeto_SQL.Models;
+
+namespace Projeto_SQL.Controllers
+{
+    [Route("[controller]")]
+    public class JogadorController : Controller
+    {
+        private readonly ILogger<JogadorController> _logger;
+
+        public JogadorController(ILogger<JogadorController> logger)
+        {
+            _logger = logger;
+        }
+
+        //instancia context
+        Context c = new Context();
+
+        [Route("Listar")]
+
+        public IActionResult Index()
+        {
+            ViewBag.Jogador = c.Jogador.ToList();
+            // ViewBag.Jogador = c.Nome.ToList();
+            ViewBag.Equipe = c.Equipe.ToList();
+
+            return View();
+        }
+
+        [Route("Excluir/{id}")]
+        public IActionResult Excluir(int id)
+        {
+            ViewBag.UserName = HttpContext.Session.GetString("UserName");
+
+            Jogador jogadorBuscado = c.Jogador.First(j => j.IdJogador == id);
+
+            c.Jogador.Remove(jogadorBuscado);
+
+            c.SaveChanges();
+
+            return LocalRedirect("~/Jogador/Listar");
+        }
+
+        [Route("Editar/{id}")]
+        public IActionResult Editar(int id)
+        {
+            Jogador jogadorBuscado = c.Jogador.First(j => j.IdJogador == id);
+
+            ViewBag.Jogador = jogadorBuscado;
+            ViewBag.Equipe = c.Equipe.ToList();
+
+            return View("Edit");
+        }
+
+        [Route("Atualizar")]
+        public IActionResult Atualizar(IFormCollection form)
+        {
+            Jogador novoJogador = new Jogador();
+
+            novoJogador.IdJogador = int.Parse(form["IdJogador"].ToString());
+            novoJogador.Nome = form["Nome"].ToString();
+            novoJogador.Email = form["Email"].ToString();
+            novoJogador.Senha = form["Senha"].ToString();
+            novoJogador.IdEquipe = int.Parse(form["IdEquipe"].ToString());
+
+            Jogador jogadorBuscado = c.Jogador.First(j => j.IdJogador == novoJogador.IdJogador);
+
+            jogadorBuscado.Nome = novoJogador.Nome;
+            jogadorBuscado.Email = novoJogador.Email;
+            jogadorBuscado.Senha = novoJogador.Senha;
+            jogadorBuscado.IdEquipe = novoJogador.IdEquipe;
+
+            c.Jogador.Update(jogadorBuscado);
+            c.SaveChanges();
+
+            return LocalRedirect("~/Jogador/Listar");
+        }
+
+        [Route("Cadastrar")]
+        public IActionResult Cadastrar(IFormCollection form)
+        {
+            Jogador novoJogador = new Jogador();
+
+            novoJogador.Nome = form["Nome"].ToString();
+            novoJogador.Email = form["Email"].ToString();
+            novoJogador.Senha = form["Senha"].ToString();
+            novoJogador.IdEquipe = int.Parse(form["IdEquipe"].ToString());
+
+            c.Jogador.Add(novoJogador);
+            c.SaveChanges();
+            return LocalRedirect("~/Jogador/Listar");
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View("Error!");
+        }
+    }
+}
